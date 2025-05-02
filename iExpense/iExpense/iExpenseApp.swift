9f4f5baec842d05c6cba1c6489f7dfd28680035c
Armaan Gupta <armaan@guppy.im>
2025-05-02 09:28:41 -0500

    ref: project 7 completed

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Armaan Gupta on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
