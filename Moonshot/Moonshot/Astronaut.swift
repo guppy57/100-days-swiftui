35e74810cdc23d8c0f78742312a3d6d9d8160cc9
Armaan Gupta <armaan@guppy.im>
2025-05-02 13:31:38 -0500

    feat: project 8

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Armaan Gupta on 5/2/25.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+	let id: String
+	let name: String
+	let description: String
+}
